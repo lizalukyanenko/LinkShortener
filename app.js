03ee11bf03a7013b439f19757074f6ba9570481c
lizamarmysh <liza.marmysh1999@gmail.com>
2019-09-11 15:55:19 +0300

    create project

---
+++ app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient;
+
+const mongoClient = new MongoClient('mongodb://localhost:27017', {useNewUrlParser: true});
+
+mongoose.connect('mongodb://localhost:27017');
+mongoose.promise = global.Promise;
+fs.readdirSync(path.join(__dirname, '/models')).forEach((
+	filename) => {
+	require(path.join(__dirname, '/models/', filename));
+});
+
+const index = require('./routes/index');
+const address = require('./routes/address');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/address', address);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
